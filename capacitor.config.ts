6a624580214b5174d7d137443a0f15dc1a6be08b
Alison Doyle <s00233102@atu.ie>
2024-09-24 19:02:08 +0100

    Added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ci.cd.test.app',
+  appName: 'ci-cd-test-app',
+  webDir: 'dist/ci-cd-test-app'
+};
+
+export default config;
